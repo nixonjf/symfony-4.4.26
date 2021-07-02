03b2541c6ff14c6b0a46d9a1199d87119146efda
Nixon <you@examm>
2021-07-02 15:53:02 +0530

    JWT register user and login

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+
+class IndexController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController {
+
+    public function redirect_exec() {
+        return JsonResponse::create(['data' => ['you have now successfully logged in!'], 'status' => Response::HTTP_OK], 200, [], true);
+    }
+
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
+        // 1) build the form
+        $user = new User();
+
+        $data = json_decode(
+                $request->getContent(), true
+        );
+
+
+//         print_r($data); die;
+        // 3) Encode the password (you could also do this via Doctrine listener)
+        $password = $passwordEncoder->encodePassword($user, $data['password']);
+
+
+        $user->setPassword($password);
+        $user->setEmail($data['username']);
+
+        // 4) save the User!
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // ... do any other work - like sending them an email, etc
+        // maybe set a "flash" success message for the user
+
+        return JsonResponse::create(['data' => ['success'], 'status' => Response::HTTP_OK], 200, [], true);
+    }
+
+}
